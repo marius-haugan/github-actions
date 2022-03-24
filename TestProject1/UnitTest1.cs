4adffd8a044c52123b7ea6ff30f1bd4e39ff755d
Marius Riis Haugan <marius.haugan@bouvet.no>
2022-03-24 11:53:26 +0100

    Add test project

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace TestProject1;
+
+public class Tests
+{
+    private int number;
+    
+    [SetUp]
+    public void Setup()
+    {
+        number = 0;
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        Assert.Fail();
+    }
+}
