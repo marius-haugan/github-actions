9a2fd32e393aa0a5d3418ed8a4b3ff5318278113
Marius Riis Haugan <marius.haugan@bouvet.no>
2022-03-24 11:57:45 +0100

    Update UnitTest1.cs

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -4,23 +4,25 @@ namespace TestProject1;
 
 public class Tests
 {
-    private int number;
+    private int _number0;
+    private int _number1;
     
     [SetUp]
     public void Setup()
     {
-        number = 0;
+        _number0 = 0;
+        _number1 = 1;
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        Assert.AreEqual(expected:0, actual:_number0);
     }
     
     [Test]
     public void Test2()
     {
-        Assert.Pass();
+        Assert.AreEqual(expected:1, actual:_number1);
     }
 }
